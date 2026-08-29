e0b7eee057aad5fc890b7075ad83264a03415fc7
Zainab Fayyaz <zainabfayyaz207@gmail.com>
2026-08-30 01:59:30 +0500

    Add error handling for invalid rows

--- first_script.py
+++ first_script.py
@@ -54,6 +54,33 @@
 #         print("Customer:", name, "Amount:", amount, "->", result)
 
 # Writing into File
+# import csv
+# def classify_order(amount):
+#     if amount>100:
+#         return "High order value"
+#     elif 20 < amount <=100:
+#         return "Medium order value"
+#     else:
+#         return "Low order value"
+
+# with open("sample_orders.csv", "r") as infile, open("classified_orders.csv","w",newline="") as outfile:
+#     reader = csv.DictReader(infile)
+#     fieldname=["order_id", "customer_name", "amount", "classification"]
+#     writer=csv.DictWriter(outfile,fieldnames=fieldname)
+#     writer.writeheader()
+
+#     for row in reader:
+#         amount = float(row["amount"])
+#         classification = classify_order(amount)
+#         writer.writerow({
+#             "order_id" : row["order_id"],
+#             "customer_name": row["customer_name"],
+#             "amount": amount,
+#             "classification": classification
+#         })
+# print("Done — check classified_orders.csv")
+
+# Try/Except
 import csv
 def classify_order(amount):
     if amount>100:
@@ -70,14 +97,18 @@ with open("sample_orders.csv", "r") as infile, open("classified_orders.csv","w",
     writer.writeheader()
 
     for row in reader:
-        amount = float(row["amount"])
-        classification = classify_order(amount)
-        writer.writerow({
-            "order_id" : row["order_id"],
-            "customer_name": row["customer_name"],
-            "amount": amount,
-            "classification": classification
-        })
+        try:
+            amount = float(row["amount"])
+            classification = classify_order(amount)
+            writer.writerow({
+                "order_id" : row["order_id"],
+                "customer_name": row["customer_name"],
+                "amount": amount,
+                "classification": classification
+            })
+            print("Customer:", row["customer_name"], ", Amount:", amount, "->", classification)
+        except ValueError:
+            print(f"Skipping invalid amount value for order_id {row['order_id']}: {row['amount']}")
+        
 print("Done — check classified_orders.csv")
-
 
